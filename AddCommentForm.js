d582d262ad6d92a71eef679c2e49cd1b26349ddd
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-03 18:57:09 -0800

    added AddCommentForm component

---
+++ AddCommentForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+class AddCommentForm extends React.Component {
+
+  render() {
+
+    return (
+      <div  >
+
+
+      </div >
+    );
+  }
+}
+
+export default AddCommentForm;
